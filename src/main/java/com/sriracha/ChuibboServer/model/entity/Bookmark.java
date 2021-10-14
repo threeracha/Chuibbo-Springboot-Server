b7f347136dc31fc31a9395c4a8e13778ec66daec
jjangdol06 <jiyeon971023@gmail.com>
2021-10-15 00:19:51 +0900

    Fix : synchronize database modeling with JPA

--- src/main/java/com/sriracha/ChuibboServer/model/entity/Bookmark.java
+++ src/main/java/com/sriracha/ChuibboServer/model/entity/Bookmark.java
@@ -1,4 +1,27 @@
 package com.sriracha.ChuibboServer.model.entity;
 
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
 public class Bookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bookmark_id")
+    private Long id;
+
+    private String area;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "job_post_id")
+    private JobPost jobPost;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/sriracha/ChuibboServer/model/entity/User.java
+++ src/main/java/com/sriracha/ChuibboServer/model/entity/User.java
@@ -32,6 +32,7 @@ import java.util.Collection;
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
     private String email;
