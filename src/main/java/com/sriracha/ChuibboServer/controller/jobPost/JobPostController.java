6d4dc2ab4d87116153517717eb56c9d72b41c044
sss20young <ksyt98@gmail.com>
2022-05-02 19:17:15 +0900

    refactor: apply try-with-resources

--- src/main/java/com/sriracha/ChuibboServer/controller/jobPost/JobPostController.java
+++ src/main/java/com/sriracha/ChuibboServer/controller/jobPost/JobPostController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -49,30 +50,32 @@ public class JobPostController {
 
         StringBuffer response = new StringBuffer();
 
-        try {
-            String text = URLEncoder.encode("", "UTF-8");
-            String apiURL = "https://oapi.saramin.co.kr/job-search?access-key="+ accessKey +"&keyword="+ text+"&count="+ count;
+        String text = URLEncoder.encode("", "UTF-8");
+        String apiURL = "https://oapi.saramin.co.kr/job-search?access-key="+ accessKey +"&keyword="+ text+"&count="+ count;
 
+        try {
             URL url = new URL(apiURL);
             HttpURLConnection con = (HttpURLConnection)url.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("Accept", "application/json");
 
             int responseCode = con.getResponseCode();
-            BufferedReader br;
 
+            InputStream inputStream;
             if(responseCode == 200) { // 정상 호출
-                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                inputStream = con.getInputStream();
             } else {  // 에러 발생
-                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                inputStream = con.getErrorStream();
             }
 
-            String inputLine;
-            while ((inputLine = br.readLine()) != null) {
-                response.append(inputLine);
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+                String inputLine;
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+            } catch (Exception e) {
+                System.out.println(e);
             }
-
-            br.close();
         } catch (Exception e) {
             System.out.println(e);
         }
